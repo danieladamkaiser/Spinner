c4efa23cb16fdcd71ee95a5f1c984ff7e4032d08
Dandan <danieladamkaiser@gmail.com>
2018-10-03 22:03:08 +0200

    spinner 1st commit

---
+++ Assets/MIDGETCONTROLLER.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MIDGETCONTROLLER : MonoBehaviour {
+
+    public AudioClip GASGASGAS;
+    public AudioSource aSource;
+    public Rigidbody fidgetRB;
+    public float angularVelocityMod;
+    public RewardController rewardController;
+
+    private Vector2 startPos;
+    private Vector2 endPos;
+    private float startTime;
+    private float spinCounter;
+    private int spinRewards;
+
+	void Start () {
+        aSource.clip = GASGASGAS;
+        aSource.volume = 0;
+        fidgetRB.maxAngularVelocity = 20;
+        Screen.orientation = ScreenOrientation.Portrait;
+	}
+	
+	void Update ()
+    {
+        spinCounter += fidgetRB.angularVelocity.magnitude*Time.deltaTime;
+        if (spinCounter*fidgetRB.angularVelocity.magnitude>4.9f)
+        {
+            spinCounter = 0;
+            rewardController.ShowNewReward(fidgetRB.angularVelocity.magnitude);
+        }
+        GetPlayerInput();
+        aSource.pitch = aSource.volume*aSource.volume + 0.1f;
+        aSource.volume = fidgetRB.angularVelocity.magnitude/15;
+	}
+
+    void Spin(float velocity)
+    {
+        if (!aSource.isPlaying)
+            aSource.Play();
+        fidgetRB.AddRelativeTorque(-(new Vector3(0, 0, velocity)), ForceMode.VelocityChange);
+        
+    }
+
+    void GetPlayerInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos =Input.mousePosition;
+            startTime = Time.realtimeSinceStartup;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            bool rotateRight = false;
+            endPos = Input.mousePosition;
+            Vector2 middle = new Vector2(Screen.width / 2, Screen.height / 2);
+            if (Vector2.SignedAngle(middle - startPos, middle - endPos) > 0) rotateRight = true;
+            Spin((rotateRight ? 1 : -1) *Vector2.Distance(startPos,endPos) / (Time.realtimeSinceStartup - startTime)/300);
+        }
+    }
+}
---
+++ Assets/RewardController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RewardController : MonoBehaviour
+{
+
+
+    public TextMeshProUGUI[] TMPros;
+    public Reward[] rewards;
+    private List<string> rewardStrings = new List<string> { "Nice!", "Great job!", "You are Awesome!", "You rock!", "Never stop spinning!", "Spin me, daddy!", "CRAZZZYYY!", "FASTER!", "COMBO X4", "+1000 POINTS", "BONUS LIFE", "Much spin!" };
+    public MinMax textTimerMinMax;
+    private void Start()
+    {
+        rewards = new Reward[TMPros.Length];
+        for (int i = 0; i < TMPros.Length; i++)
+        {
+            rewards[i] = new Reward(TMPros[i]);
+        }
+    }
+
+    private void Update()
+    {
+        foreach (Reward r in rewards)
+        {
+            r.Update(Time.deltaTime);
+        }
+    }
+
+    public void ShowNewReward(float spinMod)
+    {
+        int rewardID = FindIdleReward();
+        if (rewardID != -1)
+        {
+            rewards[rewardID].SetUpReward(Random.Range(textTimerMinMax.min, textTimerMinMax.max), spinMod, new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f)), rewardStrings[Random.Range(0, rewardStrings.Count)]);
+        }
+    }
+
+    int FindIdleReward()
+    {
+        for (int i = 0; i < rewards.Length; i++)
+        {
+            if (!(TMPros[i].IsActive()))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
+public class Reward
+{
+    private float timer = 0;
+    private float spinMod;
+    private GameObject myGO;
+    private TextMeshProUGUI myText;
+    private Vector3 direction;
+    private Vector3 startPos;
+
+    public Reward(TextMeshProUGUI go)
+    {
+        myText = go;
+        myGO = go.gameObject;
+        startPos = myGO.transform.localPosition;
+    }
+    public void SetUpReward(float timer, float spinMod, Vector3 direction, string text)
+    {
+        myGO.transform.localPosition = startPos;
+        this.timer = timer;
+        this.spinMod = spinMod;
+        this.direction = direction;
+        myText.text = text;
+        myGO.SetActive(true);
+    }
+
+    public bool Update(float deltaTime)
+    {
+        if (myGO.activeSelf)
+        {
+            if (timer <= 0)
+            {
+                Debug.Log("disabled");
+                myGO.SetActive(false);
+                return false;
+            }
+            timer -= deltaTime * spinMod;
+            myGO.transform.localScale = new Vector3(1 / (4 + timer), 1 / (4 + timer), 1 / (4 + timer)) * spinMod;
+            myGO.transform.localPosition = myGO.transform.localPosition + direction*direction.magnitude * deltaTime;
+            return true;
+        }
+        return false;
+    }
+}
+
+[System.Serializable]
+public struct MinMax
+{
+    public float min;
+    public float max;
+}
